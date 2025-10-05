5ce9d852dbb4661389bb02015da9afdb0d84f03f
Lalit <ulait9950@gmial.com>
2025-10-05 17:57:21 +0530

    Switched from MySQL to PostgreSQL for Render deploy

--- src/main/java/com/serviceprovider/BookingController.java
+++ src/main/java/com/serviceprovider/BookingController.java
@@ -3,6 +3,7 @@ package com.serviceprovider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +28,8 @@ public class BookingController {
 		
 		return serviceBooking.saveBooking(booking);
 	}
+	
+	
+
+
 }
